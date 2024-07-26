a522eddcafd181166a79a6ab6a21ff2dec6f32cf
r3dc0g <critesg@spu.edu>
2024-07-25 23:20:17 -0700

    Add feed popup

--- src/ui/components.rs
+++ src/ui/components.rs
@@ -15,7 +15,10 @@ impl WidgetRef for BlockLabel {
     fn render_ref(&self,area:Rect,buf: &mut Buffer) {
         Paragraph::new(self.label.clone())
             .alignment(Alignment::Center)
-            .block(Block::default().borders(Borders::ALL))
+            .block(
+                Block::default()
+                    .borders(Borders::ALL)
+            )
             .render(area, buf);
     }
 }
@@ -39,6 +42,46 @@ impl BlockLabel {
     }
 }
 
+#[derive(Debug, Default, Clone, Eq, PartialEq, Hash)]
+pub struct BlockText<'a> {
+    title: Option<String>,
+    paragraph: Paragraph<'a>,
+}
+
+impl<'a> BlockText<'a> {
+    pub fn title(mut self, title: Option<String>) -> Self {
+        self.title = title;
+        self
+    }
+
+    pub fn paragraph(mut self, paragraph: Paragraph<'a>) -> Self {
+        self.paragraph = paragraph;
+        self
+    }
+
+}
+
+impl<'a> WidgetRef for BlockText<'a> {
+    fn render_ref(&self, area: Rect, buf: &mut Buffer) {
+        self.paragraph.clone()
+            .block(
+                Block::default()
+                    .borders(Borders::ALL)
+                    .bg(Color::Black)
+                    .title(
+                        self.title.clone()
+                            .unwrap_or("".to_string()))
+            )
+            .render(area, buf);
+    }
+}
+
+impl<'a> Widget for BlockText<'a> {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        self.render_ref(area, buf);
+    }
+}
+
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
 pub struct ItemList<'a, T>
 where
@@ -57,10 +100,22 @@ where
     type State = ListState;
 
     fn render_ref(&self,area:Rect,buf: &mut Buffer,state: &mut Self::State) {
-        StatefulWidget::render(List::new(self.items.clone())
-            .block(Block::default().borders(Borders::ALL).title(self.title.clone().unwrap_or("".to_string())).border_style(self.style))
-            .highlight_style(Style::default().bg(Color::Red)),
-            area, buf, state);
+        StatefulWidget::render(
+            List::new(self.items.clone())
+                .block(
+                    Block::default()
+                        .borders(Borders::ALL)
+                        .title(self.title.clone().unwrap_or("".to_string()))
+                        .border_style(self.style)
+                )
+                .highlight_style(
+                    Style::default()
+                        .bg(Color::Red)
+                ),
+            area,
+            buf,
+            state
+        );
     }
 }
 
@@ -96,45 +151,72 @@ where
         self.style = style;
         self
     }
-
-    pub fn items(mut self, items: &'a T) -> Self {
-        self.items = items;
-        self
-    }
 }
 
-#[derive(Debug, Default, Clone, Eq, PartialEq, Hash)]
-pub struct BlockText<'a> {
-    title: Option<String>,
-    paragraph: Paragraph<'a>,
+pub struct Popup<W>
+where
+    W: WidgetRef {
+    height: u16,
+    width: u16,
+    inner_widget: Option<W>,
 }
 
-impl<'a> BlockText<'a> {
-    pub fn new(paragraph: Paragraph<'a>) -> Self {
+impl<W> Popup<W>
+where
+    W: WidgetRef {
+    pub fn new(inner_widget: Option<W>) -> Self {
         Self {
-            title: None,
-            paragraph,
+            height: 0,
+            width: 0,
+            inner_widget,
         }
     }
 
-    pub fn title(mut self, title: Option<String>) -> Self {
-        self.title = title;
+    pub fn height(mut self, height: u16) -> Self {
+        self.height = height;
         self
     }
 
-    pub fn paragraph(mut self, paragraph: Paragraph<'a>) -> Self {
-        self.paragraph = paragraph;
+    pub fn width(mut self, width: u16) -> Self {
+        self.width = width;
         self
     }
 }
 
-impl<'a> WidgetRef for BlockText<'a> {
-    fn render_ref(&self, area: Rect, buf: &mut Buffer) {
-        self.paragraph.clone().block(Block::default().borders(Borders::ALL).title(self.title.clone().unwrap_or("".to_string()))).render(area, buf);
+
+impl<W> Default for Popup<W>
+where
+    W: WidgetRef {
+    fn default() -> Self {
+        Self {
+            height: 0,
+            width: 0,
+            inner_widget: None,
+        }
     }
 }
 
-impl<'a> Widget for BlockText<'a> {
+impl<W> WidgetRef for Popup<W>
+where
+    W: WidgetRef {
+    fn render_ref(&self,area:Rect,buf: &mut Buffer) {
+
+        let new_area = Rect::new(
+            area.x + area.width / 2 - self.width / 2,
+            area.y + area.height / 2 - self.height / 2,
+            self.width,
+            self.height,
+        );
+
+        Clear.render_ref(new_area, buf);
+        self.inner_widget.render_ref(new_area, buf);
+
+    }
+}
+
+impl<W> Widget for Popup<W>
+where
+    W: WidgetRef {
     fn render(self, area: Rect, buf: &mut Buffer) {
         self.render_ref(area, buf);
     }
